add4b8c57bf0d8d7420b767e15e3f47daf2d33bb
walter <walter@example.com>
2014-01-15 20:59:09 -0500

    Added delete and single pages

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 from bson.objectid import ObjectId
+import datetime
 
 app = Flask(__name__)
 
@@ -11,9 +12,16 @@ def default():
     posts = db.posts.find()
     return render_template("default.html", posts=posts)
 
+@app.route('/post/<post_id>')
+def single_post(post_id):
+    mongo_client = pymongo.MongoClient("localhost", 27017)
+    db = mongo_client.ezblog
+    post = db.posts.find_one({'_id': ObjectId(post_id)})
+    return render_template('single_post_template.html', post=post)
+
 @app.route('/post/create')
 def create_post():
-    return render_template('create_post.html', action='/post/process-create-post', title='Create Post')
+    return render_template('post_template.html', action='/post/process-create-post', title='Create Post')
 
 @app.route('/post/process-create-post', methods=['POST'])
 def process_create_post():
@@ -25,7 +33,7 @@ def process_create_post():
         errors['content'] = 'The content must have 20 characters or more'
 
     if errors:
-        return render_template('create_post.html', error=errors)
+        return render_template('post_template.html', error=errors)
 
     mongo_client = pymongo.MongoClient("localhost", 27017)
     db = mongo_client.ezblog
@@ -36,8 +44,15 @@ def process_create_post():
 def edit_post(post_id):
     mongo_client = pymongo.MongoClient("localhost", 27017)
     db = mongo_client.ezblog
-    post = db.posts.find_one({'_id':ObjectId(post_id)})
-    return render_template('create_post.html', post=post, action='/post/process-edit-post', title='Edit Post')
+    post = db.posts.find_one({'_id': ObjectId(post_id)})
+    return render_template('post_template.html', post=post, action='/post/process-edit-post', title='Edit Post')
+
+@app.route('/post/process-delete-post', methods=['POST'])
+def process_delete_post():
+    mongo_client = pymongo.MongoClient("localhost", 27017)
+    db = mongo_client.ezblog
+    db.posts.remove({'_id': ObjectId(request.form['_id'])})
+    return redirect(url_for('default'))
 
 if __name__ == '__main__':
     app.run(debug=True)
